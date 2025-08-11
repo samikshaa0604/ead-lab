e92caa84438be01b00f21b138c65000aaa44d89b
Podichetty Samiksha <ugs23021_it.samiksha@cbit.org.in>
2025-08-11 21:10:47 +0530

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,10 @@
+let num = 12345; // static input
+let reversed = 0;
+
+while (num !== 0) {
+  let digit = num % 10;
+  reversed = reversed * 10 + digit;
+  num = Math.floor(num / 10);
+}
+
+console.log(`Reversed number is ${reversed}`);
