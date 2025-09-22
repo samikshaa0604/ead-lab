79aefe1029cf82a12792866f96cf8b74f71d6387
Podichetty Samiksha <ugs23021_it.samiksha@cbit.org.in>
2025-09-22 11:09:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Greet';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+  <div>
+    <h1>Welcome to React 😁</h1>
+    <Greet></Greet>
+    <Counter></Counter>
+    
+    </div>
+  );
+}
+
+export default App;
+
